35d675e5a883c6c53aca0ff3b6ef1c2849c110b2
12liyab <ktliyab7@gmail.com>
2026-01-22 17:38:21 +0000

    Update storage.ts

--- utils/storage.ts
+++ utils/storage.ts
@@ -4,7 +4,7 @@ const MEDICATIONS_KEY = "@medications";
 const DOSE_HISTORY_KEY = "@dose_history";
 
 export interface Medication {
-  id: stri;
+  id: str;
   name: string;
   dosage: string;
   times: string[];
